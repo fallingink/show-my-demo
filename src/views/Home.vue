c9b3790a779354a7d5a71d7c67105d9178f22c2d
fallingink <fallingink@ink.com>
2023-02-05 17:29:05 +0800

    init and add two demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@ import DemoEffect from "../components/DemoEffect.vue";
 import { ref } from "vue";
 const valArr: any = [Hero,DemoEffect,DemoEffect];
 const clock:object = {
-    imgurl: "/src/assets/imgs/clock.svg",
+    imgurl: "/assets/imgs/clock.svg",
     imgalt: "clock",
     title: "时钟效果展示",
     valArr: [ "采用css结合js实现", "时钟的指针采用css动画实现"],
@@ -23,7 +23,7 @@ const clock:object = {
     demoType:"clock"
 }
 const car:object ={
-    imgurl: "/src/assets/imgs/car.svg",
+    imgurl: "/assets/imgs/car.svg",
     imgalt: "car",
     title: "小车沿路线走效果展示",
     valArr: [ "采用svg技术与css3", "小车的移动采用css动画实现"],
