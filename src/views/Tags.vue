1ed1c620928532d1d45265e6ebfff289aec13194
fallingink <fallingink@ink.com>
2023-02-16 20:06:55 +0800

    feat:添加tags页面

--- src/views/Tags.vue
+++ src/views/Tags.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="">
-        待开发
+    <div :class="[tagStyles['tags-bg']]">
+        <div :class="[tagStyles['tags-balloon']]">
+            <div v-for="(tagVal,tagKey) in tags" :key="tagKey" >
+                {{ tagVal }}
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
-
+import tagStyles from "../styles/demo/__tags.module.css";
+const tags:string[] = ["car","clock"];
 </script>
